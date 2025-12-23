d8d4115735077791adbf5e923d8f724d0f9d4e4a
osamhprince <133501479+osamhprince@users.noreply.github.com>
2025-12-24 00:00:26 +0300

    bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,7 @@
 <?php
 
-// ضع التوكن الخاص بك هنا
 $API_KEY = '8539850843:AAFuOcsI8meIsm9DLd6tSHn5DYxrj4mLT98';
 
-// وظيفة لإرسال الطلبات إلى تليجرام
 function bot($method, $datas = []) {
     global $API_KEY;
     $url = "https://api.telegram.org/bot" . $API_KEY . "/" . $method;
@@ -11,15 +9,11 @@ function bot($method, $datas = []) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // لتجنب مشاكل الشهادة
     $res = curl_exec($ch);
-    if (curl_error($ch)) {
-        var_dump(curl_error($ch));
-    } else {
-        return json_decode($res);
-    }
+    return json_decode($res);
 }
 
-// استقبال البيانات القادمة من تليجرام
 $update = json_decode(file_get_contents('php://input'));
 
 if (isset($update->message)) {
@@ -28,19 +22,21 @@ if (isset($update->message)) {
     $text    = $message->text;
     $first_name = $message->from->first_name;
 
-    // الرد على أمر البداية /start
     if ($text == '/start') {
+        // تجهيز الأزرار بشكل منفصل لتجنب أخطاء السناتكس
+        $keyboard = [
+            'inline_keyboard' => [
+                [
+                    ['text' => "قناة المطور 📢", 'url' => "https://t.me/dev_osamh"],
+                    ['text' => "حساب المطور 👨‍💻", 'url' => "https://t.me/dev_osamh"]
+                ]
+            ]
+        ];
+
         bot('sendMessage', [
             'chat_id' => $chat_id,
             'text' => "أهلاً بك يا $first_name في البوت الخاص بي! 🤖",
-            'reply_markup' => json_encode([
-                'inline_keyboard' => [
-                    [
-                        ['text' => "قناة المطور 📢", 'url' => "https://t.me/dev_osamh"],
-                        ['text' => "حساب المطور 👨‍💻", 'url' => "https://t.me/dev_osamh"]"]
-                    ]
-                ]
-            ])
+            'reply_markup' => json_encode($keyboard)
         ]);
     }
 }
